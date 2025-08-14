c5764e423a2e34ff996bf7b5d1a774add769b3da
miruky <h.miyoki@gmail.com>
2025-08-14 16:50:21 +0900

    盤面サイズ定数と再開可能な乱数を公開APIに追加

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,13 @@
 export {
+  BOARD_SIZES,
   SIZE,
   TARGET_SUM,
   createRng,
   isStuck,
   move,
   newGame,
+  resumableRng,
   spawnTile,
   spawnValue,
 } from './engine';
-export type { Direction, GameState, MoveResult, Tile } from './engine';
+export type { Direction, GameState, MoveResult, ResumableRng, Tile } from './engine';
